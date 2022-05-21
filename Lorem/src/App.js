cc1cb96fc5378ddc240d603c631f282f9a45ed88
Ashraf <hassasraf@gmail.com>
2022-05-21 20:16:02 +0600

    Slider component & Lorem text generator completed

---
+++ Lorem/src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from 'react';
+import data from './data';
+function App() {
+  
+const [count, setCount] = useState(0);
+const [text, setText] = useState([])
+
+const handleSubmit = (e) =>{
+  e.preventDefault()
+  let amount = parseInt(count)
+  if(count <= 0){
+    amount = 1
+    setCount(1)
+  }
+
+  if(count > 8){
+    amount = 8
+    alert(`We have only ${amount} paragraph to show you. That's why input value redirect to the ${amount}`)
+    setCount(8)
+  }
+  setText(data.slice(0, amount))
+}
+
+  return (
+  <section className='section-center'>
+    <h3>Tired of boring lorem ipsum </h3>
+    <form className='lorem-form' onSubmit={handleSubmit}>
+      <label htmlFor='amount'>
+        Paragraphs: 
+      </label>
+      <input 
+          onChange={(e) => setCount(e.target.value)}
+          type='number' 
+          name='amount' 
+          id='amount' 
+          value={count} />
+
+          <button type='submit' className='btn'>Generate</button>
+    </form>
+    <article className='lorem-text'>
+      {text.map((item, index) =>{
+        return <p key={index}>{item}</p>
+      })}
+    </article>
+  </section>
+    )
+}
+
+export default App;
